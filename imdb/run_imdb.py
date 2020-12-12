b4380d24077e33b1e2653f36cc3b30ae4caad234
lejb <luciusbynum@lejb-pro.phinverse.com>
2020-12-11 19:11:03 -0600

    play with normalization

--- imdb/run_imdb.py
+++ imdb/run_imdb.py
@@ -160,7 +160,7 @@ lambda_coef = LAMBDA
 criterion = torch.nn.BCELoss()
 
 def clp_loss(criterion, output, labels, cf_output, lambda_coef):
-    denominator = torch.max(torch.max(output), torch.max(cf_output))
+    denominator = (output + cf_output)
     counterfactual_loss = (output - cf_output).abs().sum() / denominator
     sigmoid_out = torch.sigmoid(output)
     loss = criterion(sigmoid_out, labels) - lambda_coef * counterfactual_loss
